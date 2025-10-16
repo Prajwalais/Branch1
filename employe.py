5c1545d5ea78aaeced6dcdb1582013c9cc555595
Prajwalais <prajwalbakaningi7@gmail.com>
2025-10-16 17:25:08 +0530

    save

---
+++ employe.py
@@ -0,0 +1,6 @@
+Name = input("Enter your name: ")
+Id = int(input("Enter the employee id: "))
+Age = int(input("Enter the age: "))
+Salary = int(input("Enter the salary: "))
+
+print("Name:", Name, "Id:", Id, "Age:", Age, "Salary:", Salary)
